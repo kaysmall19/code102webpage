e7e6ae01d2eb35be3d10df5c3e87961e852c3cfe
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-07-01 13:47:04 -0500

    commit on main

--- Javascript/script-index.js
+++ Javascript/script-index.js
@@ -1,4 +1,13 @@
 alert("Welcome");
-if (confirm("are you ==21?")) {txt = "Good!";} else {txt = "No";}
-var userName= prompt("What is your name?");
-console.log(userName);
+if (confirm("are you ==21?")) {
+var txt = "Good!";
+alert{text};
+console.log{text}
+}else {
+var txt = "No";
+console.log{text};
+}
+
+function getUserAge(21=='21')
+var userAge= prompt("What is your age?");
+console.log{userAge}
