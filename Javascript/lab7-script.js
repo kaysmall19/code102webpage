c9f11e4a6b6d13fbdc5212cbdf1e73e621e1dd55
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-07-01 20:15:09 -0500

    commit on main

--- Javascript/lab7-script.js
+++ Javascript/lab7-script.js
@@ -1,8 +1,6 @@
 function guessinggame 
-var guessNumber = prompt("guess a number between 1 and 10");
+ guessNumber = prompt("guess a number between 1 and 10");
 if (
-guessed number ==9){
-return true;}
-else {
-return false;
-}
+guessedNumber==9); {
+return true};
+else {return (false);}
---
+++ Javascript/lab8-script.js
@@ -0,0 +1,6 @@
+var plates=true
+var question
+
+while(plates=true);
+question = prompt('How many plates are expected?');
+if(question =="7");
