25c692eb3d2b1846d2304b30da9869aeb67d921c
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-07-08 13:50:46 -0500

    commit on main

--- Javascript/lab7-script.js
+++ Javascript/lab7-script.js
@@ -1,4 +1,9 @@
-if(userAge < 18){
-    alert("You are not allowed in this resturant.");
-}
-console.log(userAge < 18)
+function userAge(){
+    let question = prompt("What is your age?");
+    while(question != "age"){
+        for(let i = 0; i < 18; i++){
+            question = prompt("What is your age?");
+        }
+    alert("Please enter your age");
+    }
+    console.log(userAge < 18)
