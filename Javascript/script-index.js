5dc1e65dcdcba698cab6d3d5d62c58620ab59576
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-07-08 13:55:16 -0500

    commit on main

--- Javascript/script-index.js
+++ Javascript/script-index.js
@@ -1,8 +1,16 @@
-/*if(userAge < 21){
-    if(userAge > 21){
-       
+if(userAge < 21){
+    if(userAge > 21){      
     }
     alert("How old are you?");
 }
-console.log(userAge)*/
+console.log(userAge)
 
+function userAge(){
+    let question = prompt("What is your age?");
+    while(question != "age"){
+        for(let i = 0; i < 18; i++){
+            question = prompt("What is your age?");
+        }
+    alert("Please enter your age");
+    }
+    console.log(userAge < 18)
