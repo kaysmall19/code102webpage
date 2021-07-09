f0aee696eb909420c17f1d2fe33e91984566116b
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-07-09 17:12:23 -0500

    commit on main

--- Javascript/script-index.js
+++ Javascript/script-index.js
@@ -7,10 +7,15 @@ console.log(userAge);
 
 function userAge(){
     let question = prompt("What is your age?");
-    while(question != "age"){
-        for(let i = 0; i < 18; i++){
-            question = prompt("What is your age?");
-        }
+    while(question < 21){
     alert("Please enter your age");
+    let question = prompt("What is your age?");
     }
-    console.log(userAge < 18);
+}
+userAge();
+
+var imageAmount = prompt("Enter a number");
+for(let i = 0; i < 4; i++){
+  document.write("<image src='https://images.unsplash.com/photo-1567552229523-aaa2f6b76a60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'>"); 
+  console.log(i)
+}
